35534942d87e29016e7bf8ffdd4762f59317c27c
Rafler <amuronyk@gmail.com>
2023-03-15 19:04:39 +0000

    chore: add config scripts

---
+++ common.js
@@ -0,0 +1,46 @@
+#!/usr/bin/env node
+import fs from 'node:fs';
+import * as esbuild from 'esbuild';
+
+/**
+ * Build the project using esbuild
+ *
+ * @param {object} options - The options for building the project
+ * @param {string[]} [options.entryPoints=['src/index.ts']] - An array of entry points for the build process
+ * @param {string[]} [options.target=['esnext', 'node16']] - An array of target environments for the build process
+ * @param {object} [options.config] - Additional configuration options for the build process
+ * @param {string} [options.platform='browser'] - The platform to build for (browser or node)
+ * @param {boolean} [options.metafile=false] - Whether to generate a metafile during the build process
+ */
+const build = async ({
+  entryPoints = ['src/index.ts'],
+  target = ['esnext', 'node16'],
+  config,
+  platform = 'browser',
+  metafile = false,
+}) => {
+  const result = await esbuild
+    .build({
+      logLevel: 'info',
+      entryPoints,
+      bundle: true,
+      outdir: 'lib',
+      target,
+      minify: true,
+      platform,
+      format: 'esm',
+      color: true,
+      treeShaking: true,
+      splitting: true,
+      packages: 'external',
+      metafile,
+      ...config,
+    })
+    .catch(() => process.exit(1));
+
+  if (metafile) {
+    fs.writeFileSync('meta.json', JSON.stringify(result.metafile));
+  }
+};
+
+export default build;
---
+++ frontend.js
@@ -0,0 +1,33 @@
+#!/usr/bin/env node
+import {sassPlugin} from 'esbuild-sass-plugin';
+import common from './common.js';
+
+/**
+ * Build the project using esbuild and SASS
+ *
+ * @param {object} options - The options for building the project
+ * @param {string[]} [options.entryPoints=['src/index.ts']] - An array of entry points for the build process
+ * @param {string[]} [options.target=['esnext', 'node16']] - An array of target environments for the build process
+ * @param {object} [options.config] - Additional configuration options for the build process
+ * @param {string} [options.platform='browser'] - The platform to build for (browser or node)
+ * @param {boolean} [options.metafile=false] - Whether to generate a metafile during the build process
+ * @param {object} [options.sassConfig] - Configuration options for the SASS plugin
+ */
+const build = ({
+  entryPoints = ['src/index.ts'],
+  target = ['esnext', 'node16'],
+  config,
+  platform = 'browser',
+  metafile = false,
+  sassConfig,
+}) => {
+  common({
+    entryPoints,
+    target,
+    config: {plugins: [sassPlugin(sassConfig)], ...config},
+    platform,
+    metafile,
+  });
+};
+
+export default build;
